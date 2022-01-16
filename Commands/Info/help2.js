5237b1ebcf6423db413d81393c53130f0c3ec578
Glitch (gleaming-elderly-mustard) <none>
2022-01-16 18:59:27 +0000

    🕴😼 Checkpoint ./Commands/Info/help2.js:41127513/580

--- Commands/Info/help2.js
+++ Commands/Info/help2.js
@@ -12,7 +12,8 @@ module.exports = {
       .addField("<a:se:932331677747937282> Settings Commands:", "`autorole`, `joinchannel`, `leavechannel`, `joinmessage`, `leavemessage`, `prefix`, `antilink`, `joinmessagevariables`")
       .addField("<a:fu:932335906583613531> Fun Commands:", "`8ball`, `ascii`, `cowsay`, `clyde`, `emojify`, `fliptext`, `greentext`, `orangetext`, `hack`, `hug`, `joke`, `kill`, `kiss`, `slap`, `pokeimg`, `respect`, `reverse`, `roast`, `meme`, `nitro`, `vaportext`, `urban`")
       .addField("<a:im:932339584640110633> Image Commands:", "`amazeme`, `changemymind`, `clyde`, `creatememe`, `memetemplates`, `trigger`, `trumptweet`, `wasted`, `wideavatar`")
-      .addField("<a:mo:932344516860600353> Moderation Commands:", "")
+      .addField("<a:mo:932344516860600353> Moderation Commands:", "`kick`, `ban`, `softban`, `mute`, `unmute`, `tempmute`, `emojiid`, `userinfo`, `userid`, `serverinfo`, `announce`, `clear`, `createrole`, `delrole`, `newtext`, `newvoice`, `delchannel`, `enlargeemoji`, `say`, `giverole`, `removerole`, `lock`, `unlock`, `servericon`, `suggestion`")
+      .addField("<a:mu:932347079634878555> Music Commands:", "`clearqueue`, `filter`, `filter list`, `info`, `jump`, `loop`, `lyrics`, `move`, `mute`, `pause`, `play`, `previoustrack`, `queue`, `remove`, `resume`, `unmute`, `volume`, `youtube`\nNote: Music commands work only with slash commands!")
       .addField("<:ac:932333902754906183> Activities Commands:", "`awkword`, `betrayal`, `chess`, `doodlecrew`, `fishington`, `lettertile`, `poker`, `spellcast`, `youtube`")
       .setFooter(`Requested By: ${message.author.username}`);
     message.channel.send({ embeds: [embed] });
