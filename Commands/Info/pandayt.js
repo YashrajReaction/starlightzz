5f2cfdd1fe6e08500e5a6538c2923f92cf98831b
YashrajReaction <74906098+YashrajReaction@users.noreply.github.com>
2022-01-17 11:51:13 +0530

    Update pandayt.js

--- Commands/Info/pandayt.js
+++ Commands/Info/pandayt.js
@@ -1,15 +1,24 @@
-const Discord = require("discord.js");
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
-  name: "annie",
+  name: "panda",
   description: "Get information about AnnieIzLive youtube channel",
   botPerms: ["EMBED_LINKS"],
   run: async (client, message, args) => {
-    let embed = new Discord.MessageEmbed()
-      .setTitle("AnnieIzLive's Youtube:")
+    const row = new MessageActionRow()
+			.addComponents(
+        new MessageButton()
+    .setLabel("<a:yt:932357661914198136> PandaIzLive")
+    .setStyle("LINK")
+    .setURL(`https://youtube.com/channel/UCguQBC6d05oi_AfTYwZup7Q`))
+    
+    
+    let mainPage = new MessageEmbed()
+      .setTitle("PandaIzLive's Youtube:")
       .setColor("GREEN")
-      .setDescription("")
+      .setDescription("<a:yt:932357661914198136> Youtube ➜ [PandaIzLive Youtube Channel](https://youtube.com/channel/UCguQBC6d05oi_AfTYwZup7Q)")
+      .setImage("https://media.discordapp.net/attachments/792691782399557634/932519645662019635/New_Project_17_ED5503E.gif")
       .setFooter(`Requested By: ${message.author.username}`);
-    message.channel.send({ embeds: [embed] });
+    message.channel.send({embeds: [mainPage], components: [row]})
   },
 };
