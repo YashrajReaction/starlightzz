484a9cb71c6a03ce5e357a8d47ee17d73bc914d9
YashrajReaction <74906098+YashrajReaction@users.noreply.github.com>
2022-01-17 11:39:45 +0530

    Update annieyt.js

--- Commands/Info/annieyt.js
+++ Commands/Info/annieyt.js
@@ -1,12 +1,20 @@
-const Discord = require("discord.js");
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
-  name: "yt",
+  name: "annie",
   description: "Get information about AnnieIzLive youtube channel",
   botPerms: ["EMBED_LINKS"],
   run: async (client, message, args) => {
-    let embed = new Discord.MessageEmbed()
-      .setTitle("Youtube:")
+    const row = new MessageActionRow()
+			.addComponents(
+        new MessageButton()
+    .setLabel("<a:yt:932357661914198136> AnnieIzLive")
+    .setStyle("LINK")
+    .setURL(`https://youtube.com/channel/UCDh_z_n5_2jiQDVU7BTa-1A`))
+    
+    
+    let embed = new MessageEmbed()
+      .setTitle("AnnieIzLive's Youtube:")
       .setColor("GREEN")
       .setDescription("<a:yt:932357661914198136> Youtube ➜ [AnnieIzLive Youtube Channel](https://youtube.com/channel/UCDh_z_n5_2jiQDVU7BTa-1A)")
       .setImage("https://media.discordapp.net/attachments/792691782399557634/932489078379798559/ezgif.com-gif-maker.gif")
