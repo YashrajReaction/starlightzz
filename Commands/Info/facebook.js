8b939bdec303a3a09604a0b118357c6ddf52403a
YashrajReaction <74906098+YashrajReaction@users.noreply.github.com>
2022-01-17 12:05:58 +0530

    Update facebook.js

--- Commands/Info/facebook.js
+++ Commands/Info/facebook.js
@@ -17,7 +17,7 @@ module.exports = {
       .setTitle("Starlightz's Facebook Page:")
       .setColor("GREEN")
       .setDescription("<:facebook:932521183247761429> Facebook ➜ [Starlightz Facebook Page](https://www.facebook.com/Starlightz-107905138244863/)")
-      .setImage("https://media.discordapp.net/attachments/792691782399557634/932519645662019635/New_Project_17_ED5503E.gif")
+      .setImage("https://media.discordapp.net/attachments/792691782399557634/932523397391466526/New_Project_18_3B2610E.gif")
       .setFooter(`Requested By: ${message.author.username}`);
     message.channel.send({embeds: [mainPage], components: [row]})
   },
