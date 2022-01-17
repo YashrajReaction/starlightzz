e1b60f8ebcde38b09c2aacd540aa6596bfb05069
YashrajReaction <74906098+YashrajReaction@users.noreply.github.com>
2022-01-17 11:19:01 +0530

    Update annieyt.js

--- Commands/Info/annieyt.js
+++ Commands/Info/annieyt.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports = {
-  name: "youtube2",
+  name: "yt",
   description: "Get information about AnnieIzLive youtube channel",
   botPerms: ["EMBED_LINKS"],
   run: async (client, message, args) => {
@@ -9,7 +9,7 @@ module.exports = {
       .setTitle("Youtube:")
       .setColor("GREEN")
       .setDescription("<a:yt:932357661914198136> Youtube ➜ [AnnieIzLive Youtube Channel](https://youtube.com/channel/UCDh_z_n5_2jiQDVU7BTa-1A)")
-      //.setImage("https://media.discordapp.net/attachments/792691782399557634/932489078379798559/ezgif.com-gif-maker.gif")
+      .setImage("https://media.discordapp.net/attachments/792691782399557634/932489078379798559/ezgif.com-gif-maker.gif")
       .setFooter(`Requested By: ${message.author.username}`);
     message.channel.send({ embeds: [embed] });
   },
